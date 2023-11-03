99f36ec3c22c208dcdfeb2dc8c1487bcf42c3d0b
Vinicius Oliveira <vinicius.dsro@hotmail.com>
2023-11-02 21:53:15 -0300

    finalizando o read

--- TS-Sequelize/src/app.ts
+++ TS-Sequelize/src/app.ts
@@ -1,11 +1,15 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import router from './routes'
+
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.get('/', (req, res) => res.status(200).send('Rodando...'))
-
+app.use(router)
+app.use((err: Error, req:Request, res: Response, next: NextFunction) => {
+    return res.status(500).json({message: err.message})
+}) 
 
 export default app
